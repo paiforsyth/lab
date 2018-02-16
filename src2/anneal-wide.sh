7a327a47d380950242cb382d34bd8c26deaf2cd2
paiforsyth <paiforsyth@hotmail.com>
2018-02-16 03:11:28 -0500

    Fixed Layer sizes

--- src2/anneal-wide.sh
+++ src2/anneal-wide.sh
@@ -7,7 +7,7 @@
 module purge
 module load miniconda3
 source activate pytorch
-python . --save_prefix=anneal-wide --dataset_for_classification=cifar_challenge --model_type=squeezenet --squeezenet_out_dim=100 --squeezenet_in_channels=3 --squeezenet_prop3=0.5 --squeezenet_mode=resfire --squeezenet_dropout_rate=0 --squeezenet_base=128 --squeezenet_incr=128 --squeezenet_freq=10 --squeezenet_sr=0.2 --fire_skip_mode=simple --squeezenet_num_fires=30 --squeezenet_pool_interval=10 --squeezenet_conv1_stride=1 --squeezenet_conv1_size=3 --squeezenet_num_conv1_filters=96 --squeezenet_pooling_count_offset=1 --squeezenet_use_excitation --batch_size=128 --num_epochs=300 --optimizer=sgd --sgd_momentum=0.9 --sgd_weight_decay=0.0001 --init_lr=0.2 --lr_scheduler=epoch_anneal --epoch_anneal_numcycles=6 --cuda
+python . --save_prefix=anneal-wide --dataset_for_classification=cifar_challenge --model_type=squeezenet --squeezenet_out_dim=100 --squeezenet_in_channels=3 --squeezenet_prop3=0.5 --squeezenet_mode=resfire --squeezenet_dropout_rate=0 --squeezenet_base=128 --squeezenet_incr=128 --squeezenet_freq=10 --squeezenet_sr=0.2 --fire_skip_mode=simple --squeezenet_num_fires=30 --squeezenet_pool_interval=10 --squeezenet_conv1_stride=1 --squeezenet_conv1_size=3 --squeezenet_num_conv1_filters=96 --squeezenet_pooling_count_offset=1 --squeezenet_pool_interval_mode=multiply  --squeezenet_use_excitation --batch_size=128 --num_epochs=300 --optimizer=sgd --sgd_momentum=0.9 --sgd_weight_decay=0.0001 --init_lr=0.2 --lr_scheduler=epoch_anneal --epoch_anneal_numcycles=6 #--param_report
 
 
 
