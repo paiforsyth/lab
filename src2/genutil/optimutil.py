02fcbdc364763f20ecdd9ab8ab826439cc1cbb2b
paiforsyth <paiforsyth@hotmail.com>
2018-02-16 02:08:16 -0500

    bugfix

--- src2/genutil/optimutil.py
+++ src2/genutil/optimutil.py
@@ -5,10 +5,10 @@ class MyAnneal:
         self.init_lr=init_lr
         self.Tmax=Tmax
         self.cur_step=cur_step 
-    def cur_lr(step):
-        return 0.5*self.init_lr*(1+math.cos(cur_step/Tmax*math.pi) )
+    def cur_lr(self):
+        return 0.5*self.init_lr*(1+math.cos(self.cur_step/self.Tmax*math.pi) )
     def step(self):
-        cur_step+=1
+        self.cur_step+=1
         lr=self.cur_lr()
         for param_group in self.optimizer.param_groups:
             param_group['lr']=lr
@@ -24,4 +24,4 @@ class PolyLrDecayer:
     def set_lr_poly(self, optimizer ):
         self.iter+=1
         for param_group in optimizer.param_groups:
-            param_group['lr'] = self.init_lr*(1 - self.iter/max_iter)**self.power
+            param_group['lr'] = self.init_lr*(1 - self.iter/self.max_iter)**self.power
