138eef1cdfeaf87491f4a428a8ff2216abb89138
paiforsyth <paiforsyth@hotmail.com>
2018-03-02 18:07:56 -0500

    replace slurm header in right file

--- src2/anneal-yet-wider-train-from-pyramid-dp.sh
+++ src2/anneal-yet-wider-train-from-pyramid-dp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --gres=gpu:1       # Request GPU "generic resources"
+#SBATCH --gres=gpu:lgpu:4       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=6  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=64000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=3-00:00
+#SBATCH --time=0-08:00
 #SBATCH --output=%N-%j.out
 #module purge
 #module load miniconda3
