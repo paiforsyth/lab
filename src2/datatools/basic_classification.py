2a31dd07055fe57ae000edbb13343ae3b3924585
paiforsyth <paiforsyth@hotmail.com>
2018-03-02 22:44:01 -0500

    bug fix

--- src2/datatools/basic_classification.py
+++ src2/datatools/basic_classification.py
@@ -91,7 +91,7 @@ def optimize_ensemble_on_val(contexts,val_loader):
    def eval_linear_model(model):
        combined_scores=model(score_variable).squeeze(2)
        _,predictions=torch.max(combined_scores, dim=1)
-       acc= sum(predictions.cpu() == categories.cpu() )/len(categories)
+       acc= sum(predictions.cpu() == category_variable.cpu() )/len(categories)
        return acc.data[0]
    logging.info("Equal-weight validation accuracy= "+str(eval_linear_model(meta_model)))
    optimizer=torch.optim.Adam(meta_model.parameters(),lr=0.01)
